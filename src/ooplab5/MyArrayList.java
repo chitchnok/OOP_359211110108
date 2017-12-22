11624c10e9c4f2fb6458e98488cb3c0418a875f5
LAB_COM-MT_1 <noknok220138@outlook.co.th>
2017-12-22 10:53:59 +0700

    MyArrayList

---
+++ src/ooplab5/MyArrayList.java
@@ -0,0 +1,30 @@
+package ooplab5;
+
+import java.util.ArrayList;
+
+public class MyArrayList {
+    public static void main (String[] args) {
+        //ArrayList
+        ArrayList list = new ArrayList();
+        String name = "NOK";
+        list.add(name);
+        System.out.println(list);
+        list.add("NEW");
+        list.add("DAH");
+        System.out.println(list);
+        list.add(2,"ADAM");
+        System.out.println(list);
+        list.add(1);
+        System.out.println(list);
+        list.add(10.10);
+        System.out.println(list);
+        list.remove("NEW");
+        System.out.println(list);
+        list.remove(3);
+        System.out.println(list);
+        System.out.println(list.size());
+        System.out.println(list.get(2));
+        System.out.println(list.indexOf("NEW"));
+
+    }//main
+}//class
