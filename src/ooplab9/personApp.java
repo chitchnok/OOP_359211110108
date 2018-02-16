d20c055d52f9ac4e81d64def0dfccf5ca1478e04
LAB_COM-MT_1 <noknok220138@outlook.co.th>
2018-02-16 11:27:18 +0700

    ooplab9

--- src/ooplab9/personApp.java
+++ src/ooplab9/personApp.java
@@ -3,7 +3,7 @@ package ooplab9;
 public class personApp {
     public static void main(String[] args) {
         Person person = new Person("10133", "nok",
-                new Address("11 m6", "Pattani", "98410"),
+                new Address("179 m8", "Pakphanang", "80140"),
                 new Job("Programer",45000));
 
         System.out.println(person.toString());
