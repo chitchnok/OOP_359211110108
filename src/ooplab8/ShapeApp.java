b38ea11fb066125f950a4ff60e996fc16e54877b
LAB_COM-MT_1 <noknok220138@outlook.co.th>
2018-02-16 10:42:56 +0700

    ooplab8

---
+++ src/ooplab8/ShapeApp.java
@@ -0,0 +1,13 @@
+package oop_lab8;
+
+public class ShapeApp {
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle(20, 30);
+
+        System.out.println(rectangle.findArea());
+
+        Circle circle = new Circle(20);
+        System.out.println(circle.findArea());
+    }//main
+
+}//class
